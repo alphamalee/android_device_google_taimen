f0d837f3e1b29d5ee4d0cbf9d5259a1804fb1f85
nitin1438 <nitin.chobhe@gmail.com>
2019-09-25 22:36:50 +0530

    Changes for Colt-x

--- colt_taimen.mk
+++ colt_taimen.mk
@@ -2,20 +2,17 @@
 TARGET_SCREEN_HEIGHT := 2880
 TARGET_SCREEN_WIDTH := 1440
 
-
-# Inherit Carbon GSM telephony parts
-$(call inherit-product, vendor/carbon/config/gsm.mk)
-
 # Inherit Carbon product configuration
-$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/colt/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
 -include device/google/taimen/device-carbon.mk
 
-## Device identifier. This must come after all inclusions
-PRODUCT_NAME := carbon_taimen
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := colt_taimen
+PRODUCT_DEVICE := taimen
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
 TARGET_MANUFACTURER := LGE
